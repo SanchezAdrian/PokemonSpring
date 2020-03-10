922a7b4981f8d4ce321ea262cb945bac90cdb6ec
SanchezAdrian <49305131+SanchezAdrian@users.noreply.github.com>
2020-03-10 13:01:27 +0100

    Add files via upload

--- src/main/java/example/entity/Competicion.java
+++ src/main/java/example/entity/Competicion.java
@@ -1,10 +1,33 @@
 package example.entity;
 
+import java.util.Set;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="competicion")
 public class Competicion {
+	
+	@Id
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="fecha")
+	private String fecha;
+	
+	@Column(name="lugar")
+	private String lugar;
+
+	@ManyToMany
+	Set<Equipo> EquiposCompiten;
+	
+	
 
 }
